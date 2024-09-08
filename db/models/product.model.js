96f4bd2b0622ea86bac821948d4fea13d1977168
JessCP25 <jpayanoc25@gmail.com>
2024-09-07 23:33:45 -0500

    Relacion de uno a muchos

---
+++ db/models/product.model.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const { CATEGORY_TABLE } = require('./category.model');
+
+const PRODUCT_TABLE = 'categories';
+
+const ProductSchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    priamryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: Sequelize.NOW,
+  },
+  userId: {
+    field: 'category_id',
+    allowNull: true,
+    type: DataTypes.INTEGER,
+    unique: true,
+    references: {
+      model: CATEGORY_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+  }
+}
+
+class Product extends Model{
+  static associate(models){
+    this.belongsTo(models.Category, {as: 'category'})
+  }
+
+  static config(sequelize){
+    return {
+      sequelize,
+      tableName: PRODUCT_TABLE,
+      modelName: 'Product',
+      timestamps: false,
+    }
+  }
+}
+
+module.exports = { PRODUCT_TABLE, ProductSchema, Product}
